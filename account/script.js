521da0eda9ea488442f8d96a75fa1d1b54963eae
freeutka2023 <118213546+FreeUtka2022@users.noreply.github.com>
2023-01-15 16:38:55 +0200

    Add files via upload

---
+++ account/script.js
@@ -0,0 +1,22 @@
+ const dataFromScriptJsFile = `[
+    {
+      "username": "Free_Utka",
+      "password": "1"
+    },
+    {
+      "username": "2",
+      "password": "2"
+    },
+    {
+      "username": "3",
+      "password": "3"
+    },
+    {
+      "username": "4",
+      "password": "4"
+    },
+    {
+      "username": "5",
+      "password": "5"
+    }
+  ]`;
